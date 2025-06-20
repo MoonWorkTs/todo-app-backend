d3499c7d1a26b00901bbebdd2d9967d37b097088
Дмитрий <startoff123@mail.ru>
2025-06-20 14:31:59 +0500

    add config

--- cmd/mian.go
+++ cmd/mian.go
@@ -4,13 +4,29 @@ import (
 	"log"
 	todoapp "todo-app"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatal("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todoapp.Server)
-	if err := srv.Run("8082", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal("Error occuerd while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
